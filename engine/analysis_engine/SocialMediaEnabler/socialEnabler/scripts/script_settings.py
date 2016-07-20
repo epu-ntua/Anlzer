8663afc10995ac4f67135fc1ac418b1abbb11e82
evmorfia-b <evmorfiabiliri@gmail.com>
2016-07-20 11:43:59 +0300

    update script settings file

---
+++ engine/analysis_engine/SocialMediaEnabler/socialEnabler/scripts/script_settings.py
@@ -0,0 +1,17 @@
+#twitter app
+consumer_key = ""
+consumer_secret = ""
+access_token_key = ""
+access_token_secret = ""
+
+#facebook app
+fb_access_token = ""
+
+#couchbase configuration
+cb_server = ""
+couchbase_port = 8091
+cb_admin_username = ""
+cb_admin_password = ""
+cb_twitter_bucket = ""
+cb_facebook_bucket = ""
+
